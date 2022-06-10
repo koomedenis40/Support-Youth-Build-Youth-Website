cbcf4657d4cdf0afdb11b9744a90b549c89ec0c6
KOOME <koomedenis40@yahoo.com>
2022-06-10 13:47:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<html>
+<head>
+<title>Support Youth Build Youth</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<!-----NavigationBar---->
+<section id="nav-bar">  
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="Images/logo.png"> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#updates">UPDATES</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">OUR SERVICES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">TESTIMONIALS</a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="#">OUR IMPACT</a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="#contact">CONTACT</a
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="#">DONATE</a>
+      </li>
+    </ul>
+  </div>
+</nav> 
+</section>
+<!-----Slider----->
+<div id="slider"> 
+<div id="headerSlider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#headerSlider" data-slide-to="0" class="active"></li>
+    <li data-target="#headerSlider" data-slide-to="1"></li>
+    <li data-target="#headerSlider" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/banner1.jpg" class="d-block img-fluid" alt="First slide">
+	  <div class="carousel-caption"> 
+	  <h5> We Empower</h5>
+	  </div>
+    </div>
+    <div class="carousel-item">
+      <img src="Images/banner2.jpg" class="d-block img-fluid" alt="Second slide">
+	   <div class="carousel-caption">
+	  <h5> We Support</h5>
+	  </div>
+    </div>
+    <div class="carousel-item">
+      <img src="Images/banner3.jpg" class="d-block img-fluid" alt="Third  slide">
+	  <div class="carousel-caption">
+	  <h5> We Build</h5>
+	  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#headerSlider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#headerSlider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<!----- About------>
+<section id="about">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<h2> About Us</h2>
+<div class="about-content">
+Support Youth Build Youth is piloting a program to show how intergenerational cooperation can help turn the Africa youth population bulge curse into population dividends. The program is based on the view that the population dynamics can be harnessed and nurtured through experiential leadership and engagements across ages to enhance skills and knowledge transfer, mentorship and talents development.
+<br><br><br>
+Our Vision: A United and Prosperous Youth Group with the ability to develop skills essential for strong representation and participation of youth leading to political and  socio- economic development.
+<br><br><br>
+Our Mission: To be the umbrella organisation for all independent minded African youth, providing a platform for information exchange on diverse sustainable development issues, a forum for dialogue, debate and a network of emerging and future leaders in diverse sectors.
+</div>
+<button type="button" class="btn btn-primary">Read More>></button>
+</div>
+<div class="col-md-6" skills-bar>
+<p>Advocacy</p>
+<div class="progress">
+<div class="progress-bar" style="width: 80%;">80%</div>
+</div>
+<p>Awareness</p>
+<div class="progress">
+<div class="progress-bar" style="width: 85%;">85%</div>
+</div>
+<p>Engagement</p>
+<div class="progress">
+<div class="progress-bar" style="width: 75%;">75%</div>
+</div>
+<p>API</p>
+<div class="progress">
+<div class="progress-bar" style="width: 50%;">50%</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<!-------Updates----->
+<section id="updates">
+<div class="container">
+<h1>Updates</h1>
+<div class="row updates">
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-wrench"></i>
+</div>
+<h3>Latest Youth Updates</h3>
+<p>Education Data Release: One in Every Five Children, Adolescents and Youth is Out of School</p>
+</div>
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-sign-in"></i>
+</div>
+<h3>Internship Programmes Available</h3>
+<p>Get to know what your strong skills are and get courses tailored for you to grow them.</p>
+</div>
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-line-chart"></i>
+</div>
+<h3>Voluntary Work</h3>
+<p>Volunteer in the medical and healthcare program, volunteers support the local health providers.</p>
+</div>
+<div class="col-md-3 text-center">
+<div class="icon">
+<i class="fa fa-paint-brush"></i>
+</div>
+<h3>Grow Your Skills</h3>
+<p>Get skills from IT industry experts using "GetSkills" unique Live interaction and training platform. </p>
+</div>
+</div> 
+</div>
+</section>
+<!----------Gallery------->
+<section id="gallery"> 
+<div class="container"> 
+<h1>Our Sponsers</h1>
+<div class="row">
+<div class="col-md-3 profile-pic text-center">
+<div class="img-box">
+<img src="Images/gallery0.png" class="img-responsive">
+<ul>
+<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+<a href="#"><li><i class="fa fa-envelope"></i></li></a>
+</ul>
+</div>
+<h2>KENYA NATIONAL COMMISSION FOR UNESCO</h2>
+<h4>Official Sponser</h4>
+<p>A Leading Organization In The Advancement Of Peace, Sustainable Development And Intellectual Collaboration</p>
+</div>
+<div class="col-md-3 profile-pic text-center">
+<div class="img-box">
+<img src="Images/gallery2.jpg" class="img-responsive">
+<ul>
+<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+<a href="#"><li><i class="fa fa-envelope"></i></li></a>
+</ul>
+</div>
+<h2>Cooperative Bank of Kenya</h2>
+<h4>Official Sponser</h4>
+<p>Commercial Bank in Kenya</p>
+</div>
+<div class="col-md-3 profile-pic text-center">
+<div class="img-box">
+<img src="Images/gallery2.jpg" class="img-responsive">
+<ul>
+<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+<a href="#"><li><i class="fa fa-envelope"></i></li></a>
+</ul>
+</div>
+<h2>Kenya Commercial Bank</h2>
+<h4>Official Sponser</h4>
+<p>Commercial Bank in Eastern Africa</p>
+</div>
+<div class="col-md-3 profile-pic text-center">
+<div class="img-box">
+<img src="Images/gallery2.jpg" class="img-responsive">
+<ul>
+<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+<a href="#"><li><i class="fa fa-envelope"></i></li></a>
+</ul>
+</div>
+<h2>World Bank</h2>
+<h4>Official Sponser</h4>
+<p>International Financial Institution</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-----------Promo----------->
+<section id="promo">
+<div class="container">
+<p>Support Youth & Change The Future</p>
+<a href="#" class="btn btn-primary"> Support Us</a>
+</div>
+</section> 
+<!-----------Contact-------->
+<section id="contact">
+<div class="container">
+<h1> Contact Us</h1>
+<div class="row">
+<div class="col-md-6">
+<form class="contact-form">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Your Name">
+</div>
+<form class="form-group">
+<div class="form-group">
+<input type="number" class="form-control" placeholder="Phone No">
+</div>
+<form class="form-group">
+<div class="form-group">
+<input type="email" class="form-control" placeholder="Email ID">
+</div>
+<form class="form-group">
+<div class="form-group">
+<textarea class="form-control" rows="4" placeholder=" Your Message"></textarea>
+</div>
+<button type="submit" class="btn btn-primary">Send Message</button>
+</form>
+</div>
+<div class="col-md-6 contact-info">
+<div class="follow"><b>Adress:</b><i class="fa fa-map-marker"></i>
+ Former Forthall Road, Murang'a Town, Kenya</div>
+<div class="follow"><b>Phone:</b><i class="fa fa-phone"></i> +254712756694, +25471967739</div> 
+<div class="follow"><b>Email:</b><i class="fa fa-envelope"></i> supportyouthbuidyoth@gmail.com</div>
+<div class="follow"><label><b>Get Social:</b></label> 
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-skype"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!----------Footer-------->
+<section id="footer">
+<div class="container text-center">
+<p>Support Youth Build Youth</p>
+<i class="fa fa-copyright">2020</i>
+</div>
+</section>
+<!---------Footer Close---->
+<section>
+<script src="js/smooth-scroll"></script>
+<script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+ </section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
